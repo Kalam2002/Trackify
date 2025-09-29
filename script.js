3d6aa6bcce75e9f355f481cee5f7aacae7f57de7
unknown <abdulkalamjee2021@gmail.com>
2025-09-29 13:22:43 +0530

    commit

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let box=document.getElementById("box");
 let person= ["Rohan","Kancha","Anik","Prangshu"]
+let box2=document.getElementById("box2");
 box.addEventListener("click",function(){
     let selectedPerson1=person[Math.floor(Math.random()*person.length)];
     let selectedPerson2=person[Math.floor(Math.random()*person.length)];
     while(selectedPerson1===selectedPerson2){
         selectedPerson2=person[Math.floor(Math.random()*person.length)];
     }
-    box.textContent=selectedPerson1 +" Weds "+selectedPerson2
+    box2.textContent=selectedPerson1 +" Weds "+selectedPerson2
 })
